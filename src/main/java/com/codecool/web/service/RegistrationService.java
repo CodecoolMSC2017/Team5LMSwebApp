72280f0075ba8f9ed8bdd9457e1c9ee75b830a8a
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-03-19 21:54:25 +0100

    HTML request for the required fields

--- src/main/java/com/codecool/web/service/RegistrationService.java
+++ src/main/java/com/codecool/web/service/RegistrationService.java
@@ -7,11 +7,11 @@ public final class RegistrationService {
     public Registration registration(String name, String email, String password, String confirmPassword, String role) {
         String message = "";
 
-        if ( name == "" ) { message += "Missing: name\n"; }
-        if ( email == "" )  { message += "Missing: e-mail address\n"; }
-        if ( password == "" ) { message += "Missing: password\n"; }
+//        if ( name == "" ) { message += "Missing: name\n"; }
+//        if ( email == "" )  { message += "Missing: e-mail address\n"; }
+//        if ( password == "" ) { message += "Missing: password\n"; }
         if ( confirmPassword != password ) { message += "Password does not match the confirm password.\n"; }
-        if ( role == "" ) { message += "Missing: role"; }
+//        if ( role == "" ) { message += "Missing: role"; }
 
         else message = "Successfully registered";
 
