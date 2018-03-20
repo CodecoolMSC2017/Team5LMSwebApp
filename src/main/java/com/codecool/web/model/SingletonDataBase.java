5c80a1e5bc085197d6faaaea969d0e5a2b650037
ambrusBence1 <o.g.bence@gmail.com>
2018-03-20 11:27:29 +0100

    fir SingletonDataBase with Storing interface

--- src/main/java/com/codecool/web/model/SingletonDataBase.java
+++ src/main/java/com/codecool/web/model/SingletonDataBase.java
@@ -3,10 +3,11 @@ package com.codecool.web.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SingletonDataBase {
+public class SingletonDataBase implements Storing{
 
     private static final SingletonDataBase Instance = new SingletonDataBase();
-    private static List<Registration> registrations = new ArrayList<>();
+
+    private List<Registration> registrations = new ArrayList<>();
 
     public static SingletonDataBase getInstance()
     {
@@ -16,7 +17,24 @@ public class SingletonDataBase {
     private SingletonDataBase() {
     }
 
-    public static List<Registration> getRegistrations() {
+
+    @Override
+    public List<Registration> getAllRegistration() {
         return registrations;
+    }
+
+    @Override
+    public Registration getRegistration() {
+        return null;
+    }
+
+    @Override
+    public void updateReg(Registration registration) {
+
+    }
+
+    @Override
+    public void deleteReg(Registration registration) {
+
     }
 }
