faaa4caa37782dc130c20c89a77ca842723a620e
ambrusBence1 <o.g.bence@gmail.com>
2018-03-21 13:01:09 +0100

    Storing interface ref

--- src/main/java/com/codecool/web/model/Storing.java
+++ src/main/java/com/codecool/web/model/Storing.java
@@ -1,22 +1,21 @@
 package com.codecool.web.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface Storing {
 
-    public void newLogin(Login login);
-    public Login getLogin();
-    public void delLogin();
-    public List<Registration> getAllRegistration();
-    public boolean addRegistration(Registration registration);
-    public Registration getRegistration(String name);
-    public void updateReg(Registration registration);
-    public void deleteReg(Registration registration);
-    public List<String> getNames();
-    public List<String> getEmails();
-    public List<String> getPasswords();
-    public boolean isMentor(Registration registration);
-    public List<Assignment> getAssignmentList();
+    void newLogin(Login login);
+    Login getLogin();
+    void delLogin();
+    List<Registration> getAllRegistration();
+    boolean addRegistration(Registration registration);
+    Registration getRegistration(String name);
+    void updateReg(Registration registration);
+    void deleteReg(Registration registration);
+    List<String> getNames();
+    List<String> getEmails();
+    List<String> getPasswords();
+    boolean isMentor(Registration registration);
+    List<Assignment> getAssignmentList();
 
 }
