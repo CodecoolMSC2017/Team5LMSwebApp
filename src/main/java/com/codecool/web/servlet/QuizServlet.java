0197410e42024713188aa3d59a541929eabcb69f
dnorbert4000 <dnorbert@gmail.com>
2018-03-22 14:46:56 +0100

    points as Evaluations

--- src/main/java/com/codecool/web/servlet/QuizServlet.java
+++ src/main/java/com/codecool/web/servlet/QuizServlet.java
@@ -14,8 +14,13 @@ public class QuizServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String answer1 = req.getParameter("answer1");
-        String ans = "Good answer";
-        req.setAttribute("answer1", ans);
+        int points = 0;
+        if(answer1.equals("blue")) {
+            points += 1;
+        } else {
+            points += 0;
+        }
+        req.setAttribute("answer1", points+"");
         req.getRequestDispatcher("/quizAnswer.jsp").forward(req, resp);
     }
 }
