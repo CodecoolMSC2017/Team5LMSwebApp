042d66c756b1f9b8d149d0a35bfb39845edebc7e
ambrusBence1 <o.g.bence@gmail.com>
2018-04-18 12:49:36 +0200

    userprofileservlet sql database conn impl

--- src/main/java/com/codecool/web/servlet/UserProfileServlet.java
+++ src/main/java/com/codecool/web/servlet/UserProfileServlet.java
@@ -1,5 +1,7 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.Storing;
+import com.codecool.web.dao.database.DaoDB;
 import com.codecool.web.model.Registration;
 import com.codecool.web.service.RegistrationService;
 import com.codecool.web.service.databaseServices.DatabaseRegistrationService;
@@ -11,23 +13,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/protected/userProfileServlet")
-public class UserProfileServlet extends HttpServlet{
+public class UserProfileServlet extends AbstractServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        try (Connection connection = getConnection(req.getServletContext())) {
+            Storing db = new DaoDB(connection);
+            String id = req.getParameter("id");
+            RegistrationService service = new DatabaseRegistrationService(db);
 
-        String id = req.getParameter("id");
-        RegistrationService service = new DatabaseRegistrationService();
+            if (id.equals("")) req.setAttribute("profile", req.getSession().getAttribute("user"));
+            else req.setAttribute("profile", service.getProfile(id));
 
-        if (id.equals("")) req.setAttribute("profile", req.getSession().getAttribute("user"));
-        else req.setAttribute("profile", service.getProfile(id));
+            Registration reg = (Registration) req.getSession().getAttribute("user");
+            req.setAttribute("userProfile", reg);
+            req.getRequestDispatcher("profile.jsp").forward(req, resp);
 
-        Registration reg = (Registration) req.getSession().getAttribute("user");
-        req.setAttribute("userProfile", reg);
-
-        req.getRequestDispatcher("profile.jsp").forward(req, resp);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
