9d41ca0448af51e38eda493a4cc4650c5862331c
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-04-19 12:34:39 +0200

    Assignment page from SQL

--- src/main/java/com/codecool/web/service/AssignmentService.java
+++ src/main/java/com/codecool/web/service/AssignmentService.java
@@ -2,9 +2,11 @@ package com.codecool.web.service;
 
 import com.codecool.web.model.Assignment;
 
+import java.sql.SQLException;
+
 public interface AssignmentService {
 
-    Assignment getAssignment(int id);
+    Assignment getAssignment(int id) throws SQLException;
     void update(Assignment ass, String title, String time, String shortDesc, String fullDisc);
     void delAssignment(int id);
 }
